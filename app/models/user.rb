81a046eda86599cede59d4adf03d12abcf0388c5
Ngoc Truong <ngoc.truong@posteo.de>
2019-09-05 18:45:17 +0200

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+    # We want to access the token but not save it in the database (like the password with "has_secure_password")
+    attr_accessor :remember_token 
     before_save { email.downcase! }
-
     validates :name,        presence: true, 
                             length: { maximum: 50 }
 
@@ -16,8 +17,30 @@ class User < ApplicationRecord
     has_secure_password
 
 
+    # Returns hash digest/encryption of the given string
     def User.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token/string
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # Remembers a user in the database for use in persistent sessions
+    def remember 
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Returns true if the given token matches the digest (similar to #authenticate method for passwords)
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
+    end
+
+    def forget 
+      update_attribute(:remember_digest, nil)
     end
   end
